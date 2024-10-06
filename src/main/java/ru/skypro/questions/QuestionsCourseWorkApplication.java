d65754c0727d3d899905907f92f3e0462ed966c1
tychk <semarooney@yandex.ru>
2024-10-06 11:08:39 +0300

    Coursework-questions 1st commit

---
+++ src/main/java/ru/skypro/questions/QuestionsCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsCourseWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsCourseWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/questions/QuestionsCourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsCourseWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
